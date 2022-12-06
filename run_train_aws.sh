a7828df849db22ee6ec90f9eb8fb5ac055efbeba
BK <burgerking@OUTREACHMAC-909.SCSDEAN.CS.CMU.EDU>
2022-12-05 20:52:56 -0500

    Testing-based changes to the setup_aws.sh script

--- run_train_aws.sh
+++ run_train_aws.sh
@@ -11,4 +11,4 @@ cd IFRNet
 export OMP_NUM_THREADS=1
 nohup bash scrap.sh &
 # python -m torch.distributed.launch --nproc_per_node=2 train_vimeo90k.py --world_size 2 --model_name 'IFRNet' --epochs 50 --batch_size 48  --lr_start 1e-4 --lr_end 1e-5
-python -m torch.distributed.launch --nproc_per_node=1 --master_port='29501' train_vimeo90k.py --world_size 1 --model_name 'IFRNet' --resume_epoch 34 --resume_path /content/drive/MyDrive/11785_Project/checkpoints/IFRVAN1/120422_050846/ --vimeo90k_dir /vimeo_triplet --epochs 50 --batch_size 48  --lr_start 1e-4 --lr_end 1e-5
+python -m torch.distributed.launch --nproc_per_node=1 --master_port='29501' train_vimeo90k.py --world_size 1 --model_name 'IFRNet' --resume_epoch 34 --resume_path drive/.shared/11785_Project/checkpoints/IFRVAN1/120422_050846/ --vimeo90k_dir vimeo_triplet --epochs 50 --batch_size 48  --lr_start 1e-4 --lr_end 1e-5
--- setup_aws.sh
+++ setup_aws.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
-# Install miniconda
-curl -o https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh
-bash Miniconda3-py39_4.12.0-Linux-x86_64.sh
+# Install miniconda (conda already exists on the AMI image)
+# curl -o https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh
+# bash Miniconda3-py39_4.12.0-Linux-x86_64.sh
 
 # Create environment with python 3.7 and torch
 conda create -n torch python=3.7
-conda install pytorch torchvision pytorch-cuda=11.6 -c pytorch -c nvidia
-
-# Activate the environment
 conda activate torch
-
-# Install tdqm, wandb
+conda install pytorch torchvision pytorch-cuda=11.6 -c pytorch -c nvidia
 conda install tqdm wandb
-conda deactivate
 
 # Log in to wandb
 wandb login '2fe54db29c2f3b04c92b08a6fa9872847ec64c80'
@@ -22,19 +17,14 @@ wandb login '2fe54db29c2f3b04c92b08a6fa9872847ec64c80'
 sudo add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null
 sudo apt-get update -qq 2>&1 > /dev/null
 sudo apt -y install -qq google-drive-ocamlfuse 2>&1 > /dev/null
-google-drive-ocamlfuse
+# google-drive-ocamlfuse
 sudo apt-get install -qq w3m # to act as web browser 
-xdg-settings set default-web-browser w3m.desktop # to set default browser
-cd /content
+# xdg-settings set default-web-browser w3m.desktop # to set default browser
 mkdir drive
-cd drive
-mkdir MyDrive
-cd ..
-cd ..
-google-drive-ocamlfuse /content/drive/MyDrive
+google-drive-ocamlfuse drive
 
 # Download and unzip the dataset
-cp /content/drive/MyDrive/11785_Project/vimeo_triplet.zip /vimeo_triplet.zip
+cp drive/.shared/11785_Project/vimeo_triplet.zip /vimeo_triplet.zip
 unzip -q vimeo_triplet.zip
 
 # Clone IFRNet
