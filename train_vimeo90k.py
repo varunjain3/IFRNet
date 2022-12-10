f5be79fcc1c59220fd4bfdc6d1907acd0c8fc779
BK <burgerking@OUTREACHMAC-909.SCSDEAN.CS.CMU.EDU>
2022-12-10 06:27:52 -0500

    Minor change to match reference code.

--- train_vimeo90k.py
+++ train_vimeo90k.py
@@ -127,7 +127,8 @@ def train(args, ddp_generator,model, ddp_discriminator):
             label_size = imgt_pred.size(0)
 
             # Discriminator Training Step
-            # disc_optimizer.zero_grad()
+            disc_optimizer.zero_grad()
+            gen_optimizer.zero_grad()
 
             mask = torch.ones((label_size * 2,)).to(args.device)
             mask[:label_size] = -1
@@ -142,7 +143,7 @@ def train(args, ddp_generator,model, ddp_discriminator):
             # loss_disc.backward()
             # disc_optimizer.step()
 
-            disc_optimizer.zero_grad()
+            ddp_discriminator.zero_grad()
             scaler1.scale(loss_disc).backward(retain_graph = True)
             scaler1.step(disc_optimizer)
             scaler1.update()
@@ -153,7 +154,7 @@ def train(args, ddp_generator,model, ddp_discriminator):
                 loss_adv = -torch.mean(discriminator_logits)
                 generator_loss = loss_rec + loss_kl + loss_adv
 
-                gen_optimizer.zero_grad()
+                ddp_generator.zero_grad()
                 generator_loss.backward()
                 gen_optimizer.step()
                 # scaler1.scale(generator_loss).backward()
