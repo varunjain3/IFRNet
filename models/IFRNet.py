Possible fix to gradient computation value overwrite.

models/IFRNet.py
@@ -286,11 +286,11 @@ def gradient_penalty(critic, real, fake, device):
         retain_graph = True,
     )[0]
 
-    gradient = gradient.view(gradient.shape[0], -1)
-    gradient_norm = gradient.norm(2, dim=1)
+    gradient_v = gradient.view(gradient.shape[0], -1)
+    gradient_norm = gradient_v.norm(2, dim=1)
     gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
     return gradient_penalty
-    
+
 class Generator(nn.Module):
     def __init__(self, local_rank=-1, lr=1e-4):
         super(Generator, self).__init__()